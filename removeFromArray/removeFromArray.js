b86aae37bce27f53d7f9cc435840b404386b92d7
Henry Heberle <henryhbrl@gmail.com>
2019-05-04 22:34:28 -0300

    unit test exercise

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    newArray = [];
+    list = args[0];
 
+    // solution 1
+    for (let i = 0; i < list.length; i++){
+        if (!args.includes(list[i])){
+            newArray.push(list[i]);
+        }
+    }
+
+    // solution 2
+    // list.forEach((item)=>{
+    //     if(!args.includes(item)){
+    //         newArray.push(item);
+    //     }
+    // })
+
+    return newArray;
 }
+
 
 module.exports = removeFromArray
